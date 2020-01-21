acae87c7a0611bed2760a990f33eab48e2b5e9f9
unknown <kushankur.1999@gmail.com>
2020-01-21 21:52:33 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+import 'package:ml_text_recognition/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
